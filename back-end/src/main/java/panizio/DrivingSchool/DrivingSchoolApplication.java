dd48674ef463bbd1e9579a8757442e9f87e5871b
Felipe Panizio <fpanizio10@gmail.com>
2025-02-26 15:38:52 -0300

    Initial commit

---
+++ back-end/src/main/java/panizio/DrivingSchool/DrivingSchoolApplication.java
@@ -0,0 +1,13 @@
+package panizio.DrivingSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingSchoolApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/panizio/DrivingSchool/DrivingSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package panizio.DrivingSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
